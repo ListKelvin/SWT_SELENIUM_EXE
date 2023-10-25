53d7ff8398fca6921ca53b66773ad5d0a3e4bf9e
List Kelvin <phamm5687@gmail.com>
2023-10-25 11:38:06 +0700

    Finish EXE

---
+++ src/test/java/BAITAP/TestCase01.java
@@ -0,0 +1,88 @@
+/*
+
+Test Steps
+
+Step 1. Go to http://live.techpanda.org/
+
+Step 2. Verify Title of the page
+
+Step 3. Click on -> MOBILE -> menu
+
+Step 4. In the list of all mobile , select SORT BY -> dropdown as name
+
+Step 5. Verify all products are sorted by name
+
+*/
+
+package BAITAP;
+
+import driver.driverFactory;
+import element.ElementController;
+import org.openqa.selenium.*;
+import org.openqa.selenium.io.FileHandler;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.AssertJUnit;
+
+import java.awt.*;
+import java.io.File;
+import java.io.IOException;
+
+public class TestCase01 {
+
+
+    public static void main(String[] args) {
+        WebDriver driver = driverFactory.getChromeDriver();
+
+        //Test Steps
+
+        //Step 1. Go to http://live.techpanda.org/
+        driver.get("http://live.techpanda.org/");
+        By titleSelector = By.xpath("//h2[1]");
+        By mobileSelectLink = By.linkText("MOBILE");
+
+
+        try {
+            WebElement title = driver.findElement(titleSelector);
+            //Step 2. Verify Title of the page
+            AssertJUnit.assertEquals("THIS IS DEMO SITE FOR   ", title.getText());
+            //debug purpose only
+            Thread.sleep(2000);
+            //Step 3. Click on -> MOBILE -> menu
+            driver.findElement(mobileSelectLink).click();
+            //debug purpose only
+            Thread.sleep(2000);
+            //Step 4. In the list of all mobile , select SORT BY -> dropdown as name
+            new Select(driver.findElement(By.cssSelector("select[title=\"Sort By\"] "))).selectByVisibleText("Name");
+            //Step 5. Verify all products are sorted by name
+            String dirPath = "screenshots";
+            File dir = new File(dirPath);
+            //check dir exists
+            if (!dir.exists()) {
+                dir.mkdir();
+            }
+            //casting the driver object to the TakesScreenshot interface
+            TakesScreenshot takesScreenshot = (TakesScreenshot) driver;
+            // capture a screenshot and store it in a File object.
+            File screenshot = takesScreenshot.getScreenshotAs(OutputType.FILE);
+            // creates a new File object named f by combining a directory path (dirPath) with the name of the screenshot file
+            File f = new File(dirPath + "/" + screenshot.getName());
+            //copy the screenshot file to the destination specified by the f File object.
+            FileHandler.copy(screenshot, f);
+        } catch (Error e) {
+            System.out.println("error: "+ e.toString());
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+
+        //7. Quit browser session
+        driver.quit();
+    }
+
+
+
+
+}
---
+++ src/test/java/BAITAP/TestCase02.java
@@ -0,0 +1,77 @@
+/*
+
+Test Steps:
+
+1. Goto http://live.techpanda.org/
+
+2. Click on �MOBILE� menu
+
+3. In the list of all mobile , read the cost of Sony Xperia mobile (which is $100)
+
+4. Click on Sony Xperia mobile
+
+5. Read the Sony Xperia mobile from detail page.
+
+6. Compare Product value in list and details page should be equal ($100).
+
+*/
+package BAITAP;
+
+import driver.driverFactory;
+import org.openqa.selenium.*;
+import org.openqa.selenium.io.FileHandler;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.AssertJUnit;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Objects;
+
+public class TestCase02 {
+
+    public static void main(String[] args) {
+
+        WebDriver driver = driverFactory.getChromeDriver();
+
+//        Step 1. Go to http://live.techpanda.org/
+        driver.get("http://live.techpanda.org/");
+
+//        2. Click on  MOBILE  menu
+        driver.findElement(By.linkText("MOBILE")).click();
+
+
+//      3. In the list of all mobile , read the cost of Sony Xperia mobile (which is $100)
+        String XPerialPrice = driver.findElement(By.cssSelector("span[id='product-price-1'] span[class='price']")).getText();
+
+//        4. Click on Sony Xperia mobile
+        driver.findElement(By.id("product-collection-image-1")).click();
+
+//        5. Read the Sony Xperia mobile from detail page.
+        String detailPrice = driver.findElement(By.cssSelector("span.price")).getText();
+
+//        6. Compare Product value in list and details page should be equal ($100).
+        AssertJUnit.assertEquals(XPerialPrice, detailPrice);
+        if(Objects.equals(detailPrice, XPerialPrice)) {
+            System.out.println("The price is " + detailPrice);
+        }
+        String dirPath = "screenshots";
+        File dir = new File(dirPath);
+        //check dir exists
+        if (!dir.exists()) {
+            dir.mkdir();
+        }
+        //casting the driver object to the TakesScreenshot interface
+        TakesScreenshot takesScreenshot = (TakesScreenshot) driver;
+        // capture a screenshot and store it in a File object.
+        File screenshot = takesScreenshot.getScreenshotAs(OutputType.FILE);
+        // creates a new File object named f by combining a directory path (dirPath) with the name of the screenshot file
+        File f = new File(dirPath + "/" + screenshot.getName());
+        //copy the screenshot file to the destination specified by the f File object.
+        try {
+            FileHandler.copy(screenshot, f);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        driver.quit();
+    }
+}
